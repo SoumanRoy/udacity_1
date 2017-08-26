c5631daf64d6f9f628276de0de1b6dab292e991b
Souman Roy <soumanroy.gsa@gmail.com>
2017-08-26 20:39:08 +0530

    Update media.py

--- project/media.py
+++ project/media.py
@@ -1,6 +1,8 @@
 import webbrowser
 
 class Movie():
+	""" This class stores movie related information along with ability to show a movie trailer. """
+
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
