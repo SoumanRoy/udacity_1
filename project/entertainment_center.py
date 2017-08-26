efa50688c1572b4af91f1e17d87921b374340176
Souman Roy <soumanroy.gsa@gmail.com>
2017-08-26 20:24:08 +0530

    Update entertainment_center.py

--- project/entertainment_center.py
+++ project/entertainment_center.py
@@ -1,7 +1,8 @@
 import fresh_tomatoes
 import media
 
-toy_story = media.Movie("Toy Story", "Story of a Toy Star",
+toy_story = media.Movie("Toy Story", 
+                        "Story of a Toy Star",
                         "http://vignette4.wikia.nocookie.net/disney/images/4/4c/Toy-story-movie-posters-4.jpg/revision/latest?cb=20140816182710",
                         "https://www.youtube.com/watch?v=sarLaVnB1Eo")
 
@@ -12,10 +13,11 @@ batman_vs_superman = media.Movie("Batman Vs Superman",
 deadpool = media.Movie("Deadpool", "Freakin-Sicken-comedy",
                       "http://getthatpaperson.com/wp-content/uploads/2016/02/deadpool-giveaway.jpg",
                        "https://www.youtube.com/watch?v=Xithigfg7dA")
-
-spiderman = media.Movie("Spidermen", "Spiderman-Ironman",
+spiderman = media.Movie("Spidermen", 
+                        "Spiderman-Ironman",
                         "http://cdn.collider.com/wp-content/uploads/amazing-spider-man-movie-poster.jpg",
                         "https://www.youtube.com/watch?v=yd_DIlguI6A")
 
+# This function call opens a page of movies generated
 movie = [toy_story, batman_vs_superman, deadpool, spiderman]
 fresh_tomatoes.open_movies_page(movie)
