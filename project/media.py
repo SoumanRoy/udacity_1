b22814f6abc51f4c0a864a317b1c6e840b26c153
Souman Roy <soumanroy.gsa@gmail.com>
2017-08-27 08:08:40 +0530

    Update media.py

--- project/media.py
+++ project/media.py
@@ -1,9 +1,14 @@
 import webbrowser
 
+
 class Movie():
-	""" This class stores movie related information along with ability to show a movie trailer. """
+    """ This class stores movie related information along with the ability to show a movie trailer. """
+
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        """ The input parameters are movie title, movie storyline, poster_image and youtube Url. """
+
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
